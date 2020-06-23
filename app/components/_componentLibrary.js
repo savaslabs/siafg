2b2112f461fa04da3c39cd036d9a80ef13dac938
Madeline Streilein <mcstreilein@gmail.com>
2020-06-23 09:22:39 -0400

    Issue #26321527: Update cta component to be styled based on props

--- app/components/_componentLibrary.js
+++ app/components/_componentLibrary.js
@@ -11,12 +11,12 @@ const componentLibrary = () => {
       <h2>Components</h2>
       <Card title='Just a card' />
       <CardList />
-      {/* @todo: update to use primary props. */}
-      <CTA color='white-on-white' />
-      {/* @todo: update to use secondary props. */}
-      <CTA color='white-on-gradient' />
-      {/* @todo: update to use tertiary props. */}
-      <CTA color='gradient' />
+      {/* White with purple border */}
+      <CTA primary />
+      {/* White with gradient border */}
+      <CTA secondary />
+      {/* Gradient */}
+      <CTA tertiary />
       <SearchBar />
       <Share />
     </>
--- app/components/cta.js
+++ app/components/cta.js
@@ -1,15 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const gradient1 = 'linear-gradient(135deg, #FFDD94 0%, #D1C6F3 100%)';
+
+const CTA = styled.button`
+  background: ${(props) =>
+    props.tertiary
+      ? gradient1
+      : 'white'};
+  border-radius: 3px;
+  border: ${(props) => (props.secondary ? '5px' : 'none')};
+  padding: 15px 40px;
+  position: ${(props) => (props.secondary ? 'relative' : null)};
+  ${(props) =>
+    props.secondary &&`
+      &:before {
+        content: '';
+        position: absolute;
+        top: 0; right: 0; bottom: 0; left: 0;
+        z-index: -1;
+        margin: -5px;
+        border-radius: inherit;
+        background: ${gradient1};
+    `}a
+`;
 
 // @todo update to use styled components based on props.
-const cta = ({ color }) => {
+const cta = (props) => {
+  console.log(props);
   return (
-    <button
-      className={`cta__${color}`}
+    <CTA
+      {...props}
+      {...(props.primary && { className: 'shadow' })}
       href='#'
     >
       Click me!
-    </button>
-  )
+    </CTA>
+  );
 }
 
 export default cta
