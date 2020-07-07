9b205e431b4e0fe61895add7a81c0ebbf571ef93
Madeline Streilein <mcstreilein@gmail.com>
2020-07-07 17:26:58 -0400

    Issue #26280981: Import hex constants

--- app/components/hexes.js
+++ app/components/hexes.js
@@ -1,14 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import { AppDataContext } from '../context/appDataContext';
+import { position, max, g, tangerines, ghosts, lavenders } from '../constants';
 
 const hexes = () => {
   const location = useLocation();
   const [dataPosition, setDataPosition] = useState(0);
   const appData = useContext(AppDataContext);
-  const questions = appData.questions.map(q => q.id);
-  let position = 0;
-  let max = 6;
 
   const updatePosition = n => {
     position += n;
@@ -16,7 +14,6 @@ const hexes = () => {
     setDataPosition(position);
   };
 
-  // Animate polygons on option choice.
   useEffect(() => {
     return () => {
       if (location.pathname === '/' || location.pathname === '/welcome') {
@@ -36,7 +33,7 @@ const hexes = () => {
         setDataPosition(1);
       }
     };
-  }, [location, questions]);
+  }, [location]);
 
   // Hex constructor.
   const createHex = (r, m, y, x, index) => {
@@ -49,49 +46,6 @@ const hexes = () => {
     );
   };
 
-  const g = [
-    ['tangerines', 'url(#sunnytangerine)', 0.3],
-    ['ghosts', '#F9F8FF', 0.4],
-    ['lavenders', '#F0ECFF', 0.3],
-  ];
-
-  const _tangerines = [
-    [45, 4.5, 270, 70],
-    [45, 2.5, 170, -140],
-    [20, 3.25, -220, -40],
-    [-20, 5, -200, 300],
-    [35, 3, 300, 600],
-    [50, 2.5, 80, 800],
-    [-20, 2, 330, 950],
-    [15, 5.5, -200, 1100],
-    [40, 3, 400, 1150],
-    [0, 4, 200, 1550],
-    [-15, 4, -200, 1800],
-    [15, 6, 200, 2000],
-  ];
-
-  const _ghosts = [
-    [20, 3.5, 250, -50],
-    [-20, 3, -50, 100],
-    [50, 2.5, 300, 330],
-    [10, 1.5, 100, 530],
-    [-5, 3, -100, 730],
-    [-15, 4, 200, 1050],
-    [5, 3, -80, 1250],
-    [30, 2, 400, 1450],
-    [0, 5, 100, 1750],
-  ];
-
-  const _lavenders = [
-    [5, 4, -100, -300],
-    [20, 2, 100, 200],
-    [-5, 1, 300, 500],
-    [15, 2.5, 360, 750],
-    [0, 3, -100, 950],
-    [-15, 3.5, 100, 1250],
-    [5, 5, 300, 1550],
-    [15, 4, -100, 2050],
-  ];
   return (
     <svg
       className="background"
@@ -118,11 +72,11 @@ const hexes = () => {
         return (
           <g key={i} className={type[0]} fill={type[1]} fillOpacity={type[2]}>
             {type[0] === 'tangerines' &&
-              _tangerines.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
+              tangerines.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
             {type[0] === 'ghosts' &&
-              _ghosts.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
+              ghosts.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
             {type[0] === 'lavenders' &&
-              _lavenders.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
+              lavenders.map((hex, index) => createHex(hex[0], hex[1], hex[2], hex[3], index))}
           </g>
         );
       })}
