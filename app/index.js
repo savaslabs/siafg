b46e5d7eb0f7e4323c8afc17f891344f7086a871
Alex Manzo <manzoalex17@gmail.com>
2020-07-08 10:57:28 -0400

    Issue #26511508: Add PWA manifest.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { AppDataProvider } from './context/appDataContext';
 import App from './App';
-// import * as serviceWorker from './serviceWorker';
 
 render(
   <BrowserRouter>
