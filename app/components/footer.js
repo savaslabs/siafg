e6b893265abb7fcba9759d1abaa93042ac36db31
C.C. Leonard <67921480+ccleonard@users.noreply.github.com>
2020-07-24 09:18:29 -0400

    Added footer

---
+++ app/components/footer.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import breakpoint from 'styled-components-breakpoint';
+import { routes } from '../constants';
+import footerPolygon from '../assets/footer-polygon.svg';
+
+const footer = () => {
+  return (
+    <Footer>
+      <FooterContainer>
+        <div class="container">
+          <FooterMenu>
+            <a href="https://savaslabs.com" target="_blank">
+              Savas Labs
+              <span className="sr-only">Opens in new window</span>
+            </a>
+            <img src={footerPolygon} alt="" />
+            <a href="/about">About</a>
+            <img src={footerPolygon} alt="" />
+            <a href="mailto:info@savaslabs.com">
+              Share Feedback
+              <span className="sr-only">Opens an email to info@savaslabs.com</span>
+            </a>
+          </FooterMenu>
+        </div>
+      </FooterContainer>
+    </Footer>
+  );
+};
+
+const Footer = styled.footer`
+  width: 100vw;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  ${breakpoint('lg')`
+    background: ${props => props.theme.colors.footerGradient};
+      padding: 45px 0 45px 85px;
+      z-index: 9;
+  `}
+`;
+
+const FooterContainer = styled.div`
+  width: 100%;
+  display: none;
+  justify-content: space-between;
+
+  ${breakpoint('lg')`
+    display: flex;
+  `}
+`;
+
+const FooterMenu = styled.div`
+  width: 100%;
+  min-width: 450px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default footer;
